b8de746fff2b3854fd73d408ac673d48477a9c40
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-04-26 02:05:40 +0700

    Arab Update

--- KynanRobot/config.py
+++ KynanRobot/config.py
@@ -14,28 +14,28 @@ class Config(object):
     MONGO_DB_URI = "mongodb+srv://fadhilabdat:fadhil123@cluster0.jtimqg8.mongodb.net/?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://telegra.ph//file/1d5a4b3398dc3cd1e3c0c.jpg"
+    START_IMG = ""
     
-    DONATE_LINK = "https://graph.org/file/2982a27fe0e1500bf5b17.jpg"
+    DONATE_LINK = ""
 
     SUPPORT_CHAT = "SiArab_Support"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = "6031007486:AAFMxypvl32GLsci2OWJwleD1jfvlAPg9Ig"  # Get bot token from @BotFather on Telegram
 
-    TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
+    TIME_API_KEY = "00QLM6VO01TS"  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 951454060  # User id of your telegram account (Must be integer)
+    OWNER_ID = 1345594412  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
-    DEMONS = []  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    DRAGONS = [1345594412]  # User id of sudo users
+    DEV_USERS = [1345594412]  # User id of dev users
+    DEMONS = [1345594412]  # User id of support users
+    TIGERS = [1345594412]  # User id of tiger users
+    WOLVES = [1345594412]  # User id of whitelist users
 
     ALLOW_CHATS = True
-    OWNER_USERNAME = "Riizzvbss"
+    OWNER_USERNAME = "Arabnihnge"
     ALLOW_EXCL = True
     DEL_CMDS = True
     INFOPIC = True
