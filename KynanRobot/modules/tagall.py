6ccb07a39e8dffe60701cbca393670519e9f616c
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-08-13 16:37:04 +0700

    Update tagall.py

--- KynanRobot/modules/tagall.py
+++ KynanRobot/modules/tagall.py
@@ -56,7 +56,7 @@ async def mentionall(event):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        usrtxt += f🔎"[{usr.first_name}](tg://user?id={usr.id})\n"
         if usrnum == 5:
             if mode == "text_on_cmd":
                 txt = f"{msg}\n{usrtxt}"
